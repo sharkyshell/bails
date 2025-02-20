83341eb8047d6aca2d111ade2d9d7c6b13adcc60
Davide <55992929+sharkyshell@users.noreply.github.com>
2025-02-20 19:08:10 +0100

    Update messages.d.ts

--- lib/Utils/messages.d.ts
+++ lib/Utils/messages.d.ts
@@ -40,7 +40,7 @@ export declare const extractMessageContent: (content: WAMessageContent | undefin
 /**
  * Returns the device predicted by message ID
  */
-export declare const getDevice: (id: string) => "android" | "unknown" | "web" | "ios" | "desktop";
+export declare const getDevice: (id: string) => "Android" | "Unkown" | "Web" | "iOS" | "Desktop";
 /** Upserts a receipt in the message */
 export declare const updateMessageWithReceipt: (msg: Pick<WAMessage, 'userReceipt'>, receipt: MessageUserReceipt) => void;
 /** Update the message with a new reaction */
